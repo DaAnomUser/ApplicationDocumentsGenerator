1afd50682df8d40b97bcb17591094489148e9874
DaAnomUser <DaAnomUser@gmail.com>
2018-09-29 17:38:27 +0200

    V 0.0.0.1 Initial Version

---
+++ ApplicationDocumentsGenerator/MainForm.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace bewerbungsemailer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SelectFilesButton = new System.Windows.Forms.Button();
+            this.openFileDialogExcel = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialogPDF = new System.Windows.Forms.SaveFileDialog();
+            this.CreatePdfsButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.openFileDialogWord = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // SelectFilesButton
+            // 
+            this.SelectFilesButton.Location = new System.Drawing.Point(1088, 25);
+            this.SelectFilesButton.Name = "SelectFilesButton";
+            this.SelectFilesButton.Size = new System.Drawing.Size(152, 37);
+            this.SelectFilesButton.TabIndex = 0;
+            this.SelectFilesButton.Text = "Select files";
+            this.SelectFilesButton.UseVisualStyleBackColor = true;
+            this.SelectFilesButton.Click += new System.EventHandler(this.SelectFilesButton_Click);
+            // 
+            // openFileDialogExcel
+            // 
+            this.openFileDialogExcel.FileName = "openFileDialogXlsx";
+            // 
+            // CreatePdfsButton
+            // 
+            this.CreatePdfsButton.Location = new System.Drawing.Point(1088, 63);
+            this.CreatePdfsButton.Name = "CreatePdfsButton";
+            this.CreatePdfsButton.Size = new System.Drawing.Size(152, 37);
+            this.CreatePdfsButton.TabIndex = 1;
+            this.CreatePdfsButton.Text = "Create PDF files";
+            this.CreatePdfsButton.UseVisualStyleBackColor = true;
+            this.CreatePdfsButton.Click += new System.EventHandler(this.CreatePdfsButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(212, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Excel file with table of textblocks:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(111, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Word sourcefile:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(228, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(854, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(228, 52);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(854, 22);
+            this.textBox2.TabIndex = 6;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.SelectFilesButton);
+            this.groupBox1.Controls.Add(this.CreatePdfsButton);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(2, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1246, 114);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Selected files:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox3);
+            this.groupBox2.Location = new System.Drawing.Point(2, 132);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(1246, 314);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Output:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(6, 21);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(1234, 286);
+            this.textBox3.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(80, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(142, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Target PDF filename:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(228, 78);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(854, 22);
+            this.textBox4.TabIndex = 8;
+            // 
+            // openFileDialogWord
+            // 
+            this.openFileDialogWord.FileName = "openFileDialogXlsx";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1250, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.Text = "ApplicationDocumentsGenerator";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SelectFilesButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialogExcel;
+        private System.Windows.Forms.SaveFileDialog saveFileDialogPDF;
+        private System.Windows.Forms.Button CreatePdfsButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.OpenFileDialog openFileDialogWord;
+    }
+}
+
---
+++ ApplicationDocumentsGenerator/MainForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace bewerbungsemailer
+{
+    public partial class MainForm : Form
+    {
+        readonly SautinSoft.PdfMetamorphosis pdfMetamorphosis = new SautinSoft.PdfMetamorphosis();
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        void SelectFilesButton_Click(object sender, EventArgs e)
+        {
+            openFileDialogWord.ShowDialog();
+            //saveFileDialogPDF.ShowDialog();
+        }
+
+        void CreatePdfsButton_Click(object sender, EventArgs e)
+        {
+            var docxPath = openFileDialogWord.FileName;
+            var pdfPath = Path.ChangeExtension(docxPath, ".pdf");
+
+            var retVal = pdfMetamorphosis.DocxToPdfConvertFile(docxPath, pdfPath);
+
+            if (retVal == 0)
+                System.Diagnostics.Process.Start(pdfPath);
+            else
+                MessageBox.Show("Conversion failed!");
+        }
+    }
+}
